e77eb66626a0ff6acd4be48cd347a2731993e30d
neeraj rawat <98204838+Neerajrawat123@users.noreply.github.com>
2024-07-20 16:17:42 +0530

    Update Card.tsx

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {type ReactNode} from 'react'
 
-function Card({children}:{children: ReactNode, }) {
+function Card({children}:{children: ReactNode}) {
   return (
     <div className='py-8 text-center px-[50px] border border-card-border rounded-[20px]'>{children}</div>
   )
